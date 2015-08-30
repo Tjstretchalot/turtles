8caa3672997d49f44200ae140c332800d27f751d
Cyenth <stephen.moore00@gmail.com>
2015-08-30 12:59:14 -0700

    removed debug stuff

--- wall.lua
+++ wall.lua
@@ -1,19 +1,14 @@
 dofile('pathfinding.lua')
 dofile('inventory.lua')
 local function doColumn(height, colNum)
-	print('Function started')
 	local desiredZ = colNum - 1
-	print('Going to ' .. desiredZ)
 	pathfinding.gotoZXY(desiredZ, 0, 0)
-	print('Facing North')
 	move.face(position.NORTH)
-	print('Starting Column')
 	for i = 1, height do
 		inventory.selectItem('minecraft:stonebrick')
 		turtle.place()
 		move.up()
 	end
-	print('Done with column ' .. colNum)
 end
 print('How long should the wall be?')
 local wallLength = tonumber(io.read())
@@ -22,5 +17,3 @@ local wallHeight = tonumber(io.read())
 for i = 1, wallLength do
 	doColumn(wallHeight, i)
 end
-pathfinding.goto(0, 0, 0)
-move.face(position.NORTH)
