34521abd9aa84bd1065c24386ff2c8fc50314458
Cyenth <stephen.moore00@gmail.com>
2015-08-29 18:21:22 -0700

    Made treefarm actually farm
    Will go to tree, cut down, replant, go to next tree, repeat

--- treefarm.lua
+++ treefarm.lua
@@ -1,5 +1,8 @@
 dofile('pathfinding.lua')
-
+dofile('inventory.lua')
+local desiredCount = 16
+local logChestLoc = {-1, 0, 1}
+local saplingChestLoc = {-3, 0, 1}
 local function isTree()
 	local success, data = turtle.inspect()
 	if success then
@@ -7,7 +10,6 @@ local function isTree()
 	end
 	return false
 end
-
 local function cutTree()
 	if not isTree() then return end
 	turtle.dig()
@@ -20,6 +22,38 @@ local function cutTree()
 	pathfinding.gotoY(curY)
 	move.back()
 end
+local function gotoTree(i, k)
+	local desiredX = i * 4 - 4
+	local desiredZ = -k * 4
+	pathfinding.gotoXZY(desiredX + 1, position.y, desiredZ + 1)
+	pathfinding.gotoXZY(desiredX, position.y, desiredZ + 1)
+	move.face(position.NORTH)
+end
+local function returnLogs()
+	pathfinding.goto(logChestLoc[1], logChestLoc[2], logChestLoc[3])
+	inventory.dumpInventory('minecraft:log', nil, turtle.dropDown)
+end
+local function plantSapling()
+	inventory.selectItem('minecraft:sapling', nil)
+	turtle.place()
+end
+local function getSaplings()
+	pathfinding.goto(saplingChestLoc[1], saplingChestLoc[2], saplingChestLoc[3])
+	inventory.acquireItem('minecraft:sapling', 2, desiredCount, turtle.suckDown, turtle.dropDown)
+end
 
+while true do
+	for i = 1, 3 do
+		for j = 1, 3 do
+			gotoTree(i, j)
+			cutTree()
+			plantSapling()
+		end
+	end
+	returnLogs()
+	getSaplings()
+	pathfinding.goto(0, 0, 0)
+	move.face(position.NORTH)
+	os.sleep(30)
+end
 
-cutTree()
