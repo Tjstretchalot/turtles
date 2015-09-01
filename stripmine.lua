5b035a50d9da82ac1da2c5356bc0f40c7b50ffc7
Cyenth <stephen.moore00@gmail.com>
2015-09-01 14:12:11 -0700

    Made work better, fixed math

--- stripmine.lua
+++ stripmine.lua
@@ -1,6 +1,6 @@
 dofile('tunnel.lua')
 
-local function goToColumn(colNum)
+local function goToColumn(colNum, rowNumTotal)
 	local neededDir = false
 	local desiredZ = 0
 	if colNum%2 == 1 then
@@ -8,36 +8,42 @@ local function goToColumn(colNum)
 		desiredZ = 0
 	else
 		neededDir = position.SOUTH
-		desiredZ = -12
+		desiredZ = -4 * (rowNumTotal - 1)
 	end
 	local desiredX = 4 * (colNum - 1)
-	print('Going to ' ..desiredX .. ', ' ..desiredZ)
 	pathfinding.gotoXZY(desiredX, 0, desiredZ)
 	move.face(neededDir)
 end
-local function goToRow(rowNum)
+local function goToRow(rowNum, colNumTotal)
 	if rowNum % 2 == 1 then
 		desiredDir = position.WEST
-		desiredX = 12
+		desiredX = 4 * (colNumTotal - 1)
 	else
 		desiredDir = position.EAST
 		desiredX = 0
 	end
-	desiredZ = 4 * (rowNum - 1)
-	pathfinding.gotoZXY(desiredZ, desiredX, 0)
+	desiredZ = -4 * (rowNum)
+	pathfinding.gotoZXY(desiredX, 0, desiredZ)
 	move.face(desiredDir)
 end
-local function doColumns(colNumTotal, height, length)
+local function doColumns(colNumTotal, rowNumTotal, height)
 	for i = 1, colNumTotal do
-		goToColumn(i)
-		tunnel.doTunnel(1, height, length)
+		goToColumn(i, rowNumTotal)
+		tunnel.doTunnel(1, height, 4 * (rowNumTotal - 1))
 	end
 end
-local function doRows(rowNumTotal, height, length)
+local function doRows(rowNumTotal, colNumTotal, height)
 	for i = 1, rowNumTotal do
-		gotoRow(i)
-		tunnel.doTunnel(1, height, length)
+		goToRow(i, colNumTotal)
+		tunnel.doTunnel(1, height, 4 * (colNumTotal - 1))
 	end
 end
-doColumns(2, 3, 9)
+print('How many rows of pillars?')
+local totalRowNumber = tonumber(io.read())
+print('How many columns of pillars?')
+local totalColNumber = tonumber(io.read())
+print('Doing a '..totalRowNumber * 4 ..' by '..totalColNumber * 4 ..' room')
+doColumns(totalColNumber + 1, totalRowNumber, 3)
+doRows(totalRowNumber, totalColNumber + 1, 3)
 pathfinding.goto(0, 0, 0)
+move.face(position.NORTH)
